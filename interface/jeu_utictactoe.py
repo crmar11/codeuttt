b395a65998b6bc0761eba3c39879aa363cad2b99
KAMARA <ibraim.kamara.1@ulaval.ca>
2015-04-15 22:27:49 -0400

    La date a été changée.

--- interface/jeu_utictactoe.py
+++ interface/jeu_utictactoe.py
@@ -1,5 +1,5 @@
-__authors__ = Cristi Margineanu et Ibrahim Kamara
-__date__ = "Ajoutez la date de remise"
+__authors__ = "Cristi Margineanu et Ibrahim Kamara"
+__date__ = "15 avril 2015"
 
 """Ce fichier permet de...(complétez la description de ce que
 ce fichier est supposé faire ! """
